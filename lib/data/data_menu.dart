49e917753ac2a330c8cbcb48bdd6ca9aa4e698ee
satriarikhan <satriarikhan@gmail.com>
2025-09-30 09:26:15 +0700

    bgf

--- lib/data/data_menu.dart
+++ lib/data/data_menu.dart
@@ -4,11 +4,7 @@ List<Menu> menuList = [
   Menu(
     name: 'Nasi Goreng',
     price: 'Rp 20.000',
-    imageUrls: [
-      'https://cdn.akamai.steamstatic.com/steam/apps/265930/header.jpg',
-      'https://cdn.akamai.steamstatic.com/steam/apps/265930/ss_0f0e9008ba21b5eed106147cdc355d3c7bad683a.jpg',
-      'https://cdn.akamai.steamstatic.com/steam/apps/265930/ss_25b9da1037082d6a7c3c12e2f3a7707c5c2e721e.jpg',
-    ],
+    imageUrls: ['assets/images/fotoku.jpg'],
   ),
 
   Menu(
